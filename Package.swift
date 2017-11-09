e1d0be1ba9ac9f4a18543cb2872bc29ed01853fc
Kyle Jessup <kyle@perfect.org>
2017-11-09 09:59:20 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
 		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 3),
 		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 3),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),

51db8ee8a0b03733cb0be57b530518fee3d7eceb
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 15:35:09 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,21 +1,29 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "Perfect-LocalAuthentication-PostgreSQL",
-    targets: [
-		Target(
-			name: "PerfectLocalAuthentication",
-			dependencies: []
-		)
-		],
+	products: [
+		.library(name: "Perfect-LocalAuthentication-PostgreSQL", targets: ["Perfect-LocalAuthentication-PostgreSQL"])
+	],
     dependencies: [
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
-		]
-
+		.package(url: "https://github.com/iamjono/JSONConfig.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "Perfect-LocalAuthentication-", dependencies: [
+			"JSONConfig",
+			"PerfectRequestLogger",
+			"PerfectSMTP",
+			"PostgresStORM",
+			"PerfectSessionPostgreSQL",
+			"PerfectMustache",
+			"PerfectHTTP"
+			])
+	]
 )
